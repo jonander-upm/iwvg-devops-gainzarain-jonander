831c06a99c3d28706beae504bec6b36cea7212ec
jonander-upm <jonander.gainzarain@alumnos.upm.es>
2022-10-04 13:52:52 +0200

    Added UsersDatabase #6

--- src/main/java/es/upm/miw/iwvg_devops/code/Fraction.java
+++ src/main/java/es/upm/miw/iwvg_devops/code/Fraction.java
@@ -2,6 +2,14 @@ package es.upm.miw.iwvg_devops.code;
 
 public class Fraction {
 
+    public Fraction() {
+
+    }
+
+    public Fraction(int numerator, int denominator) {
+
+    }
+
     @Override
     public String toString() {
         return "Fraction{}";
